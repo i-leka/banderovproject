c96a45207a20522b3cf53dc8dda562336a2d6d9c
i.le.ka <zaliibragimova@yandex.ru>
2022-10-31 20:16:30 +0300

    My project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'banderovproject';
+  products: {id: number; name: string; count: number; price: number; sum: number}[] = [
+    {id: 1, name: 'TovarName1', count: 1, price: 10, sum: 10},
+    {id: 2, name: 'TovarName1', count: 0, price: 10, sum: 0},
+    {id: 3, name: 'myTovar3', count: 1, price: 15, sum: 15},
+    {id: 4, name: 'NewTovar5', count: 0, price: 15, sum: 0},
+  ];
+
+  addProduct(id: number): void {
+    this.products.map((product) => {
+      if (product.id === id) {
+        product.count++;
+        product.sum += product.price;
+      }
+    })
+  }
+
+  delProduct(id: number): void {
+    this.products.map((product) => {
+      if (product.id === id) {
+        if (product.count === 0) {
+          return;
+        }
+        product.count--;
+        product.sum -= product.price;
+      }
+    })
+  }
 }
